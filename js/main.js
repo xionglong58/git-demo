6f417651b15b7e676b2d412f38341b9dee606f3c
xionglong-mac <xionglong0106@gmail.com>
2019-10-16 21:41:28 +0800

    首次提交

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            margin: 10,
+            nav: true,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true
+        }
+    );
+});
